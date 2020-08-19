a24eee0c08e5f11599fed15228c31b922d82032e
Baris Tikir <brstkr@icloud.com>
2020-08-19 16:36:11 +0200

    Refactoring Setup

--- front-end/gui/src/store/actions/actionTypes.js
+++
@@ -1,8 +0,0 @@
-// -- This JS File is for all type of actions -- //
-  // --   actions like logs in, logs out   -- //
-
-// Action Types
-export const AUTH_START = 'AUTH_START';
-export const AUTH_SUCCESS = 'AUTH_SUCCESS';
-export const AUTH_FAIL = 'AUTH_FAIL';
-export const AUTH_LOGOUT = 'AUTH_LOGOUT';
